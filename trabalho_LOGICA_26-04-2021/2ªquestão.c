84e053127474fb9ce55e9ed0d2b50929e37c7713
Emanoel Lucas <emanoellucas123456@gmail.com>
2021-04-26 16:12:43 -0300

    2ª questão

---
+++ trabalho_LOGICA_26-04-2021/2ªquestão.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    float soma_areas = 0, raio, area;
+    int quantidade_areas = 0;
+    const int PI = 3.141592;
+
+    do{
+        printf("Digite o raio: ");
+        scanf("%f", &raio);
+        setbuf(stdin, NULL);
+        if(raio >= 0){
+            area = PI * raio;
+            printf("Area: %.2f.\n\n\n", area);
+            soma_areas = soma_areas + area;
+            quantidade_areas++;
+        }
+    }while(raio >= 0);
+    printf("\n\nMedia das areas: %.2f.\n", soma_areas/quantidade_areas);
+
+    return 0;
+}
